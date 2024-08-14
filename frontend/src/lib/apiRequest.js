76f2bdc123a16d8fe74f54d54c6540fc0b7f0297
bhavWeb <164836723+bhavWeb@users.noreply.github.com>
2024-08-15 00:42:45 +0530

    Update apiRequest.js with actual backend url

--- frontend/src/lib/apiRequest.js
+++ frontend/src/lib/apiRequest.js
@@ -1,10 +1,10 @@
 import axios from "axios"
 
 
-const baseURL = import.meta.env.VITE_BACKEND_API_URL;
+// const baseURL = import.meta.env.VITE_BACKEND_API_URL;
 
 const apiRequest = axios.create({
-    baseURL : baseURL
+    baseURL : "https://classroom-web-app.onrender.com"
 })
 
 export default apiRequest
