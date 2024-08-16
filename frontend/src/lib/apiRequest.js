786d7aa7bca171e86002a226284ed3c826b2deec
bhavWeb <164836723+bhavWeb@users.noreply.github.com>
2024-08-16 23:36:24 +0530

    Update apiRequest.js
    updated the baseUrl in apiRequet.js to the deployed backendUrl/api

--- frontend/src/lib/apiRequest.js
+++ frontend/src/lib/apiRequest.js
@@ -4,7 +4,7 @@ import axios from "axios"
 // const baseURL = import.meta.env.VITE_BACKEND_API_URL;
 
 const apiRequest = axios.create({
-    baseURL : "https://classroom-web-app.onrender.com"
+    baseURL : "https://classroom-web-app.onrender.com/api"
 })
 
 export default apiRequest
